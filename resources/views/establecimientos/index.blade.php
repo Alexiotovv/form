91c80b4569698b754abaab39d5816537de7253a0
Alexiotovv <gpsalexvasquez@gmail.com>
2025-09-01 13:16:49 -0500

    cant_intentos_envio

--- resources/views/establecimientos/index.blade.php
+++ resources/views/establecimientos/index.blade.php
@@ -71,6 +71,7 @@
                         <select name="envios" id="envios" name ="envios" class="form-select">
                             <option value="1">1</option>
                             <option value="2">2</option>
+                            <option value="3">3</option>
                         </select>
                     </div>
                 </div>
@@ -107,6 +108,7 @@
                         <select name="envios" name ="envios" id="editEnvios"  class="form-select">
                             <option value="1">1</option>
                             <option value="2">2</option>
+                            <option value="3">3</option>
                         </select>
                     </div>
                 </div>
